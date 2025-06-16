9beda85645c429b367ff25872c403796361c5523
brooklyns-games <62125537+brooklyns-games@users.noreply.github.com>
2025-06-16 13:49:13 -0700

    Shape Filters

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import random
 import pygame
 import pymunk
 
+"""Started June 14 2025
+from Pymunk Basics by Ear of Corn Programming
+
+
+"""
+
+
 pygame.init()
 W, H = 800, 700
 DISPLAY = pygame.display.set_mode((W, H))
@@ -24,16 +31,17 @@ def flip(x, y, null=False):
         return x, y
 
 bodies = pygame.sprite.Group()
+joints = pygame.sprite.Group()
 
 
 class BodySprite(pygame.sprite.Sprite):
     body_num = 0
-    def __init__(self, x, y, mass, collision_type, color=(255, 0, 0), category=pymunk.Body.DYNAMIC):
+    def __init__(self, x, y, mass=0, collision_type=0, color=(255, 0, 0), category=pymunk.Body.DYNAMIC):
         super().__init__(bodies)
         self.color = color
 
         self.m = mass
-        self.body = pymunk.Body(self.m, 100, category)
+        self.body = pymunk.Body(self.m, body_type=category)
         self.body.position = self.x, self.y = flip(x, y)
 
         self.shape = pymunk.Circle(self.body, 10)  # pymunk.Poly(self.body, [(-1, 1), (1, 1), (1, -1), (-1, -1)])
@@ -50,7 +58,7 @@ class BodySprite(pygame.sprite.Sprite):
 
 
 class Ball(BodySprite):
-    def __init__(self, x, y, r, collision_type=0, color=(255, 0, 0)):
+    def __init__(self, x, y, r, collision_type=0, group=0, color=(255, 0, 0)):
         super().__init__(x, y, 5, collision_type, color=color)
         # self.body.velocity = random.uniform(-500, 500), random.uniform(-500, 500)
 
@@ -63,32 +71,41 @@ class Ball(BodySprite):
         self.shape.elasticity = 1
 
         self.shape.collision_type = collision_type  # shape.collsiion_type is *separate* from init
+        self.shape.filter = pymunk.ShapeFilter(group=group)
 
     def draw(self, s):
-        if self.shape.collision_type == 2:
-            self.color = (0, 0, 255)
-        else:
-            self.color = (255, 0, 0)
+        # if self.shape.collision_type == 2:
+        #     self.color = (0, 0, 255)
+        # else:
+        #     self.color = (255, 0, 0)
         pygame.draw.circle(s, self.color, (self.x, self.y), self.radius)
 
-    def tagged(self, arbiter, space, data):
-        self.shape.collision_type = 2
-        # return True
+    # def tagged(self, arbiter, space, data):
+    #     self.shape.collision_type = 2
+    #     # return True
 
 
-class Floor(BodySprite):
-    def __init__(self, x1, y1, x2, y2):
-        super().__init__(x1, y1, category=pymunk.Body.STATIC)
+class Platform(BodySprite):
+    def __init__(self, x1, y1, x2, y2, color=(0, 0, 0), group=0):
+        super().__init__(x1, y1, color=color, category=pymunk.Body.STATIC)
+        print(self.body)
 
-        self.x1, self.y1 = self.start = x1, y1
-        self.x2, self.y2 = self.end = x2, y2
-        print(self.start, self.end)
+        self.x1, self.y1 = x1, y1
+        self.x2, self.y2 = x2, y2
+        self.start, self.end = 0, 0
 
-        self.shape = pymunk.Segment(self.body, self.start, self.end, 5)
+        self.shape = pymunk.Segment(self.body, (0, 0), (600, 0), 5)
         self.shape.elasticity = 1
+        self.shape.density = 1
+        self.shape.filter = pymunk.ShapeFilter(group=group)
+        # SPACE.add(self.body, self.shape)
+    def update(self):
+        self.start = flip(self.x1, self.y1)
+        self.end = flip(self.x2, self.y2)
 
     def draw(self, s):
         pygame.draw.line(s, self.color, self.start, self.end, 5)
+
 
 def collide(arbiter, space, data):
     print('hello')
@@ -97,19 +114,17 @@ def collide(arbiter, space, data):
 # floor = Floor(0, 700, 800, 100)
 # ball1 = Ball(200, 600, 10, 1)
 # ball2 = Ball(500, 600, 10, 2)
-class Rope:
+class Rope(pygame.sprite.Sprite):
     def __init__(self, body, attachment):
+        super().__init__(joints)
         self.body1 = body
         if isinstance(attachment, pymunk.Body):
-            print('body', attachment)
             self.body2 = attachment
 
         elif isinstance(attachment, tuple):
-            print('coords', attachment)
             self.body2 = pymunk.Body(body_type=pymunk.Body.STATIC)
             self.body2.position = flip(*attachment)
-        joint = pymunk.PinJoint(self.body1, self.body2)
-        SPACE.add(joint)
+        self.joint = pymunk.PinJoint(self.body1, self.body2)
 
         self.start = self.body1.position
         self.end = self.body2.position
@@ -119,7 +134,6 @@ class Rope:
         self.end = self.body2.position
 
     def draw(self, s):
-        print(self.start, self.end)
         pygame.draw.line(s, (0, 0, 0), self.start, self.end, 3)
 
 
@@ -128,13 +142,20 @@ def main():
     # balls = [Ball(random.randint(0, W), random.randint(0, H), 10, i + 3) for i in range(200)]
     # balls.append(Ball(400, 400, 10, 2, ))
     #
-    ball1 = Ball(300, 600, 10,)
-    ball2 = Ball(200, 150, 10, )
-    rope1 = Rope(ball1.body, (300, 550))
-    rope2 = Rope(ball1.body, ball2.body)
+    ball1 = Ball(150, 600, 10, color=(255, 0, 0), group=1)
+    ball2 = Ball(300, 600, 10, color=(0, 255, 0), group=1)
+    ball3 = Ball(450, 600, 10, color=(0, 0, 255), group=1)
+    platform1 = Platform(0, 400, W, 400, (0, 0, 0), group=1)
+    platform2 = Platform(0, 100, W, 100, (128, 64, 0), group=2)
+
+    # rope1 = Rope(ball1.body, (300, 550))
+    # rope2 = Rope(ball1.body, ball2.body)
 
     for sprite in bodies:
+        print(sprite.body, sprite.shape)
         SPACE.add(sprite.body, sprite.shape)
+    for sprite in joints:
+        SPACE.add(sprite.joint)
     #
     #
     # handlers = [SPACE.add_collision_handler(2, i + 3) for i in range(200)]
@@ -148,21 +169,16 @@ def main():
                 return
 
         bodies.update()
-        rope1.update()
-        rope2.update()
-        # print(bodies)
+        joints.update()
 
+        # images, blitting
         DISPLAY.fill('white')
 
         for body in bodies:
+            # print(body)
             body.draw(DISPLAY)
-        rope1.draw(DISPLAY)
-        rope2.draw(DISPLAY)
-
-        # x, y = body.position
-        # pygame.draw.circle(DISPLAY, 'red', (round(x), int(y)), 10)
-        # DISPLAY.blit(ball1.image, (ball1.x, ball1.y), )
-
+        for joint in joints:
+            joint.draw(DISPLAY)
 
         pygame.display.update()
         CLOCK.tick(60)
